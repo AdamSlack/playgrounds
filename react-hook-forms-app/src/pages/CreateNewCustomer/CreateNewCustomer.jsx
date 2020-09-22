a2a949712fea8dee4d201fed35b5befa14c8adc1
Adam Slack <adamslack@outlook.com>
2020-09-22 17:54:32 +0100

    basic react-hook-form with fields conditional on a dropdown

---
+++ react-hook-forms-app/src/pages/CreateNewCustomer/CreateNewCustomer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CreateNewCustomerForm from '../../Forms/CreateNewCustomerForm/CreateNewCustomerForm'
+
+const CreateNewCustomer = () => {
+    return (
+        <>
+            <CreateNewCustomerForm />
+        </>
+    )
+}
+
+export default CreateNewCustomer
